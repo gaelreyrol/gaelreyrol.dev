a678bf3380f0f6fc6cc9d721a6a90d355db0c3be
Gaël Reyrol <me@gaelreyrol.dev>
2020-12-08 09:56:29 +0100

    feat: update layout with avatar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <header class="p-8 flex">
+      <NuxtLink to="/">
+        <img
+          class="rounded-full h-12 sm:h-16 lg:h-24"
+          src="~/static/avatar.jpeg"
+          alt="Gaël Reyrol"
+        />
+      </NuxtLink>
+    </header>
     <Nuxt />
   </div>
 </template>

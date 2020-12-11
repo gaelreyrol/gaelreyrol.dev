c1b219397d5419967c7604070d47513d895e0b92
Gaël Reyrol <gael@dansmaculotte.fr>
2020-12-11 11:24:20 +0100

    fix(styles): make header responsive

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
-    <header class="p-8 flex items-center space-x-8">
+    <header
+      class="p-8 flex flex-col items-center space-y-2 sm:space-x-8 sm:space-y-0 sm:flex-row"
+    >
       <NuxtLink to="/" active-class="bg-gray-900 p-px rounded-full">
         <img
-          class="rounded-full h-12 sm:h-16 lg:h-24"
+          class="rounded-full w-12 h-12 sm:h-16 sm:w-16 lg:h-24 lg:w-24"
           src="~/static/avatar.jpeg"
           alt="Gaël Reyrol"
         />
       </NuxtLink>
-      <a
-        class="text-sm sm:text-md lg:text-xl"
-        href="https://onestla.tech/"
-        target="_blank"
-        rel="noopener"
-      >
-        #OnEstLaTech
-      </a>
       <NuxtLink
         to="/about"
         active-class="underline"
@@ -30,9 +24,17 @@
       >
         Ma stack
       </NuxtLink>
+      <a
+        class="text-sm sm:text-md lg:text-xl"
+        href="https://onestla.tech/"
+        target="_blank"
+        rel="noopener"
+      >
+        #OnEstLaTech
+      </a>
     </header>
     <Nuxt class="px-4 pb-16" />
-    <footer class="p-4 border-t-2">
+    <footer class="p-4 border-t-2 text-sm sm:text-md lg:text-xl">
       Fait avec <strong>Nuxt.js</strong> & <strong>Tailwind CSS</strong> /
       Hébergé par <strong>Clever Cloud</strong>
     </footer>

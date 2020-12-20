c2cf7b8fb811d7b69359b0e080c12f077a12f5ce
Gaël Reyrol <me@gaelreyrol.dev>
2020-12-20 12:33:58 +0100

    chore(nuxt): remove hash router

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,12 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 
-  router: {
-    mode: 'hash'
+  generate: {
+    async routes() {
+      const { $content } = require('@nuxt/content')
+      const files = await $content({ deep: true }).only(['path']).fetch()
+
+      return files.map((file) => (file.path === '/index' ? '/' : file.path))
+    }
   }
 }

bf0608983d5c80635d2caa2688628b5ed6a02b2c
gaelreyrol <gaelreyrol@users.noreply.github.com>
2022-05-30 08:54:35 +0000

    deploy: 9aa042fea8372ed55d66c422e6829f4bcc6ed53f

---
+++ _nuxt/static/1653900874/projects/game-of-life/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653900874",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fgame-of-life",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e8b39cff"}}}}(null));

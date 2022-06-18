f41a000401c2e3926466eeb28b6742b4d0e0029d
gaelreyrol <gaelreyrol@users.noreply.github.com>
2022-06-18 11:16:14 +0000

    deploy: ba631f3e08be454f77d860592a344ac0c92d2d2d

--- _nuxt/static/1654180861/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fprojects\u002Fderbyhouse","\u002Fstack","\u002Fabout","\u002Fprojects\u002Fgame-of-life","\u002Fprojects"]})
--- _nuxt/static/1654180861/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654180861",layout:"default",error:a,serverRendered:true,routePath:b,config:{app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f02a3dae"}}}}(null,"\u002F"));

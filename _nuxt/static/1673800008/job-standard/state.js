a4bc55e181920afb64f7af25e3c75d37d7523daa
gaelreyrol <gaelreyrol@users.noreply.github.com>
2023-01-15 16:31:25 +0000

    deploy: c40eb5c36ff2153a56272b3a6c422f66023b15c0

--- _nuxt/static/1673800008/job-standard/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673800008",layout:"default",error:a,serverRendered:true,routePath:"\u002Fjob-standard",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"22506f93"}}}}(null));

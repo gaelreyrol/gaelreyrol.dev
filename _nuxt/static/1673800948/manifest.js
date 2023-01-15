fdfe191ed70fa72dc79d77885eecfb69971f53e5
gaelreyrol <gaelreyrol@users.noreply.github.com>
2023-01-15 16:42:30 +0000

    deploy: 9adb22aed1aa5cf1759a2f4782868ec936468b27

---
+++ _nuxt/static/1673800948/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fsalary-transparency","\u002Fstack","\u002Fabout","\u002Fprojects\u002Fderbyhouse","\u002Fprojects\u002Fgame-of-life","\u002Fprojects","\u002Fjob-standard"]})
---
+++ _nuxt/static/1673800948/projects/derbyhouse/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673800948",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects\u002Fderbyhouse",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a57b7845"}}}}(null));

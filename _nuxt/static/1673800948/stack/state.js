04f35310137f045b9463f5b9c6748ddd43920a2d
gaelreyrol <gaelreyrol@users.noreply.github.com>
2023-01-16 15:26:50 +0000

    deploy: 6375b1218ec5aa9d89c4ca3facb33a7ac612648b

--- _nuxt/static/1673800948/stack/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673800948",layout:"default",error:a,serverRendered:true,routePath:"\u002Fstack",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"a57b7845"}}}}(null));

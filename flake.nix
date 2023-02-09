aad35ed1518a51b9aeaefa92977729f48cec5051
Gaël Reyrol <me@gaelreyrol.dev>
2023-02-09 15:42:20 +0100

    feat: add nix flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Zola development environment";
+  description = "Zola development environment with Tailwind and Dhall";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
@@ -10,16 +10,41 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        buildPackages = with pkgs; [
+          zola
+          nodejs
+          nodePackages.npm
+          dhall-json
+        ];
+        src = ./.;
+        npmPackageLock = builtins.fromJSON (builtins.readFile (src + "/package-lock.json"));
       in
       {
         devShells = {
           default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              zola
-              nodejs
-              nodePackages.npm
-              dhall-json
-            ];
+            packages = [] ++ buildPackages;
+          };
+        };
+        packages = flake-utils.lib.flattenTree {
+          default = pkgs.buildNpmPackage {
+            inherit (npmPackageLock) name version;
+            inherit src;
+
+            npmBuildScript = "styles:build";
+            npmDepsHash = "sha256-6N3y2OPmSpgWrbIPS3JJ1i06icZGq4dx+zMUCiM7mRE=";
+
+            nativeBuildInputs = [] ++ buildPackages;
+
+            installPhase = ''
+              runHook preInstall
+
+              # dhall-to-json --file dhall/resume.dhall --output static/resume.json
+              # cat static/resume.json | node_modules/.bin/jsonresume-theme-even > static/resume.html
+              zola build
+              cp -r public $out
+
+              runHook postInstall
+            '';
           };
         };
       });
